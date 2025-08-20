06daccd96de363e2c64886450ed58ed9978dd2b7
Kseniia <boriksu@mail.ru>
2025-08-21 00:20:01 +0300

    fix order modal: add svg, add shadow, centering

--- src/components/BurgerConstructor/BurgerConstructorOrder/OrderDetails/OrderDetails.jsx
+++ src/components/BurgerConstructor/BurgerConstructorOrder/OrderDetails/OrderDetails.jsx
@@ -1,13 +1,22 @@
 import PropTypes from "prop-types";
 import naming from "../../../../data/ru.json";
+import check from "../../../../images/check.svg";
+import styles from "./OrderDetails.module.css";
 
 const OrderDetails = ({ orderNumber }) => {
   return (
     <>
-      <p className="mb-8 text text_type_digits-large">{orderNumber}</p>
+      <p className={`${styles.orderNumber} text text_type_digits-large mb-8`}>
+        {orderNumber}
+      </p>
       <p className="mb-15 text-center text text_type_main-medium">
         {naming.OrderDetails.orderNumber}
       </p>
+      <img
+        src={check}
+        className={`${styles.image} mb-15 text-center`}
+        alt="Ваш заказ принят"
+      />
       <p className="mb-2 text-center text text_type_main-default">
         {naming.OrderDetails.cooking}
       </p>
