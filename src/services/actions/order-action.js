78473ccbdc67a99d4f4f51934733d93a50afa8c9
Kseniia <boriksu@mail.ru>
2025-09-19 16:18:38 +0300

    small fix

--- src/services/actions/order-action.js
+++ src/services/actions/order-action.js
@@ -1,4 +1,4 @@
-import { createOrder } from "../../data/orderCreate";
+import { createOrder } from "../../data/api/createOrder";
 
 export const ORDER_ACTIONS = {
   CREATE_REQUEST: "CREATE_REQUEST",
@@ -12,7 +12,10 @@ export function orderAction(ingredients) {
     dispatch({ type: ORDER_ACTIONS.CREATE_REQUEST });
     createOrder(ingredients)
       .then((data) => {
-        dispatch({ type: ORDER_ACTIONS.CREATE_SUCCESS, orderNumber: data });
+        dispatch({
+          type: ORDER_ACTIONS.CREATE_SUCCESS,
+          orderNumber: data.order.number,
+        });
       })
       .catch((err) => {
         dispatch({ type: ORDER_ACTIONS.CREATE_FAILURE });
