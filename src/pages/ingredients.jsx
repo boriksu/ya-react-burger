7141a168a4917005b0639393840cfe9b0ea2f16b
Kseniia <boriksu@mail.ru>
2025-09-19 16:22:04 +0300

    add IngredientPage

---
+++ src/pages/ingredients.jsx
@@ -0,0 +1,13 @@
+import IngredientDetails from "../components/BurgerIngredients/BurgerIngredientsItem/IngredientDetails/IngredientDetails";
+
+function IngredientPage() {
+  return (
+    <main className="page-container">
+      <div className="page-container-inner">
+        <IngredientDetails />
+      </div>
+    </main>
+  );
+}
+
+export default IngredientPage;
