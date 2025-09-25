c17cfb2236349332e3dba97c9703baedaa23fd01
Kseniia <boriksu@mail.ru>
2025-09-25 15:10:39 +0300

    profile-orders page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,7 +5,7 @@ import MainPage from "./main.jsx";
 import Page404 from "./page404";
 import ProfileEdit from "./profile-edit";
 import ProfileLogout from "./profile-logout";
-import ProfileOrders from "./profile-orders.jsx";
+import ProfileOrders from "./profile-orders";
 import Profile from "./profile.jsx";
 import Register from "./register.jsx";
 import ResetPassword from "./reset-password.jsx";
