b7567f05e3da38cb66f606eece09b5ceb46a345b
Kseniia <boriksu@mail.ru>
2025-10-16 23:05:11 +0300

    change selectors

---
+++ src/services/selectors.ts
@@ -0,0 +1,11 @@
+import { RootState } from "./store";
+
+export const getAuth = (state: RootState) => state.auth;
+export const getIngredients = (state: RootState) => state.loadIngredients;
+export const getConstructor = (state: RootState) => state.burgerConstructor;
+export const getOrder = (state: RootState) => state.createOrder;
+export const getDisplayedIngredient = (state: RootState) =>
+  state.ingredientWindow.displayedIngredient;
+export const getTab = (state: RootState) => state.tabInfo.tab;
+export const getOrdersAll = (state: RootState) => state.ordersAll;
+export const getOrdersUser = (state: RootState) => state.ordersUser;
